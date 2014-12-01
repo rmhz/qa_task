82f6aa46aa9f54341a689849df1e30a3936f7f3b
MRR work PC <rmukhametzyanov@cogniance.com>
2014-12-01 18:39:07 +0200

    Rewrite with unittest lib

--- Script1 - OK level.py
+++ Script1 - OK level.py
@@ -1,100 +1,59 @@
-import json
-import httplib
+import httplib, unittest, json
 
-conn = httplib.HTTPConnection("qainterview.cogniance.com")
-try:
-    conn.connect()
-except:
-    raise Exception("Connection error")
+def getCandidatesList(connection):
+    connection.request("GET", "/candidates")
+    response = connection.getresponse()
+    return {'status':response.status, 'header':response.getheaders(), 'body':response.read()}
+
+def getCandidate(connection, cand_id):
+    connection.request("GET", "/candidates/"+str(cand_id))
+    response = connection.getresponse()
+    return {'status':response.status, 'header':response.getheaders(), 'body':response.read()}
+
+def postCandidate(connection, header, body):
+    connection.request("POST", "/candidates", body, header)
+    response = connection.getresponse()
+    return {'status':response.status, 'header':response.getheaders(), 'body':response.read()}
+
+def deleteCandidate(connection, cand_id):
+    connection.request("DELETE", "/candidates/"+str(cand_id))
+    response = connection.getresponse()
+    return {'status':response.status, 'header':response.getheaders(), 'body':response.read()}    
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        self.connection = httplib.HTTPConnection("qainterview.cogniance.com")
+        try:
+            self.connection.connect()
+        except:
+            raise Exception("Connection error")
+
+    def tearDown(self):
+        self.connection.close()
+
+class GetTest(SimpleTest):
+    def test_getCandidatesList(self):
+        self.assertEqual(getCandidatesList(self.connection)['status'], 200, 'GET test fail')
+
+    def test_validCandidateAdd(self):
+        body = json.dumps({'name': "name_test", 'position': "pos_test"})
+        header = {'Content-Type': 'application/json'}
+        response = postCandidate(self.connection, header, body)
+        self.assertEqual(response['status'], 201, 'POST test fail')
+        return json.loads(response['body'])['candidate']['id']
+
+    def test_getCandidate(self):
+        cand_id = self.test_validCandidateAdd()
+        self.assertEqual(getCandidate(self.connection, cand_id)['status'], 200)
+
+    def test_dataAddedToServer(self):
+        init_length = len(getCandidatesList(self.connection)['body'])
+        self.test_validCandidateAdd()
+        self.assertGreater(len(getCandidatesList(self.connection)['body']), init_length)
     
+    def test_deleteCandidate(self):
+        cand_id = self.test_validCandidateAdd()
+        self.assertEqual(deleteCandidate(self.connection, cand_id)['status'], 200)
 
-###test1 - GET Method
-#Reques a list of candidates, expect status 200
-conn.request("GET", "/candidates")
-r1 = conn.getresponse()
-status = r1.status
-init_cand_len = len(r1.read())
-if status == 200:
-    print 'GET /candidates method -> OK'
-else:
-    print 'GET /candidates method -> Fail'
-
-
-###test2 - POST Method
-#Add new valid candidate, expect status 201
-body = json.dumps({'name': "name_test", 'position': "pos_test"})
-header = {'Content-Type': 'application/json'}
-conn.request("POST", "/candidates", body, header)
-r2 = conn.getresponse()
-status = r2.status
-r2.read()
-if status == 201:
-    print 'POST new candidate method -> OK'
-else:
-    print 'POST new candidate method -> Fail'
-
-###test2_1
-#Add new invalid candidate - name absent, expect status 400
-body = json.dumps({'position': "pos_test"})
-header = {'Content-Type': 'application/json'}
-conn.request("POST", "/candidates", body, header)
-r21 = conn.getresponse()
-status = r21.status
-r21.read()
-if status == 400:
-    print 'POST new candidate without name method -> OK'
-else:
-    print 'POST new candidate without name method -> Fail'
-
-###test2_2
-#Add new invalid candidate - content-type absent, expect status 400
-body = json.dumps({'name': "name_test", 'position': "pos_test"})
-header = {}
-conn.request("POST", "/candidates", body, header)
-r22 = conn.getresponse()
-status = r22.status
-r22.read()
-if status == 400:
-    print 'POST new candidate without header method -> OK'
-else:
-    print 'POST new candidate without header method -> Fail'
-
-
-###test3
-#Request list of candidates after addition of new candidate
-#Expect length of data increased -> some data added to server
-#Getting an id of last added candidate
-conn.request("GET", "/candidates")
-r3 = conn.getresponse()
-resp_data = r3.read()
-new_cand_len = len(resp_data)
-candidates = json.loads(resp_data)['candidates']
-cand_id=candidates[-1:][0]['id']
-if new_cand_len > init_cand_len:
-    print 'Some data added to server -> OK'
-else:
-    print 'Some data added to server -> Fail'
-
-###test4
-#Request candidate data by <cand_id>, expect status 200
-conn.request("GET", "/candidates/"+str(cand_id))
-r4 = conn.getresponse()
-status = r4.status
-r4.read()
-if status == 200:
-    print 'GET /candidates/<cand_id> method -> OK'
-else:
-    print 'GET /candidates/<cand_id> method -> Fail'
-
-###test5
-#Delete a candidate with <cand_id>, expect status 200
-conn.request("DELETE", "/candidates/"+str(cand_id))
-r5 = conn.getresponse()
-status = r5.status
-r5.read()
-if status == 200:
-    print 'DELETE /candidates/<cand_id> method -> OK'
-else:
-    print 'DELETE /candidates/<cand_id> method -> Fail'
-
-conn.close()
+if __name__ == '__main__':
+    unittest.main()
