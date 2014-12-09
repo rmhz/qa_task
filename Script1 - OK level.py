b9cf3323ea4b33d0056ad47e0dc4f60ece7befd3
MRR work PC <rmukhametzyanov@cogniance.com>
2014-12-09 18:51:58 +0200

    Rewrite with pytest

--- Script1 - OK level.py
+++ Script1 - OK level.py
@@ -1,4 +1,4 @@
-import httplib, unittest, json
+import httplib, pytest, json
 
 def getCandidatesList(connection, header={}, body=""):
     connection.request("GET", "/candidates", body, header)
@@ -20,102 +20,102 @@ def deleteCandidate(connection, cand_id, header={}, body=""):
     response = connection.getresponse()
     return {'status':response.status, 'header':response.getheaders(), 'body':response.read()}    
 
-class SimpleTest(unittest.TestCase):
-    def setUp(self):
+
+class TestFixture():
+    def setup_method(self, method):
         self.connection = httplib.HTTPConnection("qainterview.cogniance.com")
         try:
             self.connection.connect()
         except:
             raise Exception("Connection error")
 
-        self.body_list = [json.dumps({}),
-                     json.dumps({'position': "pos_test"}),
-                     json.dumps({'name': "name_test"}),
-                     json.dumps({'position': "P"*257}),
-                     json.dumps({'name': "N"*257}),
-                     json.dumps({'name': "n"*257, 'position': "p"*257})]
-        self.header_list = [{},
-                           {'Content-Type': 'application/javascript'},
-                           {'Content-Type': 'application/json'},
-                            {'Content-Type': 'text/html'}]
-
-        self.longmessage = True
-
-    def tearDown(self):
+    def teardown_method(self, method):
         self.connection.close()
 
-class GetTest(SimpleTest):
+class TestQA(TestFixture):
     def test_getCandidatesList(self):
-        self.assertEqual(getCandidatesList(self.connection)['status'], 200, 'GET test fail')
+        assert (getCandidatesList(self.connection)['status'] == 200)
 
     def test_validCandidateAdd(self):
         body = json.dumps({'name': 'name_test', 'position': "pos_test"})
         header = {'Content-Type': 'application/json'}
         response = postCandidate(self.connection, header, body)
-        self.assertEqual(response['status'], 201, 'POST test fail')
-##        print response
+        assert (response['status'] == 201)
         return json.loads(response['body'])['candidate']['id']
 
     def test_getCandidate(self):
         cand_id = self.test_validCandidateAdd()
-        self.assertEqual(getCandidate(self.connection, cand_id)['status'], 200)
+        assert (getCandidate(self.connection, cand_id)['status'] == 200)
 
     def test_dataAddedToServer(self):
         init_length = len(getCandidatesList(self.connection)['body'])
         self.test_validCandidateAdd()
-        self.assertGreater(len(getCandidatesList(self.connection)['body']), init_length)
-    
+        assert (len(getCandidatesList(self.connection)['body']) > init_length)
+
     def test_deleteCandidate(self):
         cand_id = self.test_validCandidateAdd()
         response = deleteCandidate(self.connection, cand_id)
 ##        print response
-        self.assertEqual(response['status'], 200)
+        assert (response['status'] == 200)
 
+body_list = [json.dumps({}),
+             json.dumps({'position': "pos_test"}),
+             json.dumps({'name': "name_test"}),
+             json.dumps({'position': "P"*257}),
+             json.dumps({'name': "N"*257}),
+             json.dumps({'name': "n"*257, 'position': "p"*257})]
+header_list = [{},
+                   {'Content-Type': 'application/javascript'},
+                   {'Content-Type': 'application/json'},
+                    {'Content-Type': 'text/html'}]
 
-class AdvTest(SimpleTest):
-       
-    def test_invalidCandidateAdd(self):
-        header_body_list = list((x,y) for x in self.header_list for y in self.body_list)
-        for hb_pair in header_body_list:
-            self.assertEqual(postCandidate(self.connection, *hb_pair)['status'], 400, str(hb_pair))
+header_body_list = list((x,y) for x in header_list for y in body_list)
+
+def pytest_generate_tests(metafunc):
+    idlist=[]
+    argvalues=[]
+    i=0
+    try:
+        for hb_pair in metafunc.cls.header_body_list:
+           idlist.append(i)
+           argnames = ["header","body"]
+           argvalues.append(hb_pair)
+           i+=1
+        metafunc.parametrize(argnames, argvalues, ids=idlist, scope="function")
+    except:
+        pass
+
+class TestQAAdv(TestFixture):
+    header_body_list = list((x,y) for x in header_list for y in body_list)
+    
+    def test_invalidCandidateAdd(self, header, body):
+        assert (postCandidate(self.connection, header, body)['status'] == 400)
 
     def test_invalidCandidateGet(self):
-        self.assertEqual(getCandidate(self.connection, -1)['status'], 404)
+        assert (getCandidate(self.connection, -1)['status'] == 404)
 
-    def test_invalidCandidateGet_withBH(self):
-        header_body_list = list((x,y) for x in self.header_list for y in self.body_list)
-        for hb_pair in header_body_list:
-            self.assertEqual(getCandidate(self.connection, -1, *hb_pair)['status'], 404, str(hb_pair))
+    def test_invalidCandidateGet_withBH(self, header, body):
+        assert (getCandidate(self.connection, -1, header, body)['status'] == 404)
 
     def test_invalidCandidateDelete(self):
-        self.assertEqual(deleteCandidate(self.connection, -1)['status'], 404)
+        assert (deleteCandidate(self.connection, -1)['status'] == 404)
 
     def test_noCandidateDelete(self):
-        self.assertEqual(deleteCandidate(self.connection, None)['status'], 404)
+        assert (deleteCandidate(self.connection, None)['status'] == 404)
                  
-    def test_invalidCandidateDelete_withBH(self):
-        header_body_list = list((x,y) for x in self.header_list for y in self.body_list)
-        for hb_pair in header_body_list:
-            self.assertEqual(deleteCandidate(self.connection, -1, *hb_pair)['status'], 404, str(hb_pair))
+    def test_invalidCandidateDelete_withBH(self, header, body):
+        assert (deleteCandidate(self.connection, -1, header, body)['status'] == 404)
 
-    def test_validCandidateDelete_withBH(self):
-        body = json.dumps({'name': 'name_test', 'position': "pos_test"})
-        header = {'Content-Type': 'application/json'}
-        response = postCandidate(self.connection, header, body)
+    def test_validCandidateDelete_withBH(self, header, body):
+        valid_body = json.dumps({'name': 'name_test', 'position': "pos_test"})
+        valid_header = {'Content-Type': 'application/json'}
+        response = postCandidate(self.connection, valid_header, valid_body)
         cand_id = json.loads(response['body'])['candidate']['id']
-        header_body_list = list((x,y) for x in self.header_list for y in self.body_list)[1:]
-        for hb_pair in header_body_list:
-            self.assertEqual(deleteCandidate(self.connection, cand_id, *hb_pair)['status'], 404, str(hb_pair))
+        assert (deleteCandidate(self.connection, cand_id, header, body)['status'] == 404)
 
     def test_deleteCandidatesList(self):
         self.connection.request("DELETE", "/candidates")
         response = self.connection.getresponse()
         result = {'status':response.status, 'header':response.getheaders(), 'body':response.read()}
-        self.assertEqual(result['status'], 405)
-            
-
-suite = unittest.TestLoader().loadTestsFromTestCase(GetTest)
-suite.addTest(unittest.TestLoader().loadTestsFromTestCase(AdvTest))
-unittest.TextTestRunner(verbosity=2).run(suite)
-
+        assert (result['status'], 405)
 
